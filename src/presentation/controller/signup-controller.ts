d2045c5532dc74bdc8f5510b690fecfb1f199b5f
G4br1elM4t0s <gabrielmatos010203@gmail.com>
2023-07-31 17:02:38 -0300

    feat: should return a 400 error if the email is invalid

--- src/presentation/controller/signup-controller.ts
+++ src/presentation/controller/signup-controller.ts
@@ -1,17 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import { InvalidParamError } from "../erros/invalid-param-error"
 import { MissingParamError } from "../erros/missing-param-error"
 import { badRequest } from "../helpers/http-helper"
+import { EmailValidator } from "../protocols/email-validator"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignupController {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ["email", "password", "name", "confirmPassword"]
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const { email, password, name, confirmPassword } = httpRequest.body
+    if (password !== confirmPassword) {
+      return badRequest(new InvalidParamError("passwordConfirmation"))
+    }
+
+    const isValid = this.emailValidator.isValid(email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"))
     }
 
     return {
---
+++ src/presentation/erros/invalid-param-error.ts
@@ -0,0 +1,6 @@
+export class InvalidParamError extends Error {
+  constructor(message: string) {
+    super(`invalid param: ${message}`)
+    this.name = "InvalidParamError"
+  }
+}
